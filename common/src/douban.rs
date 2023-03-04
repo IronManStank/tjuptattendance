4d5e0e1c0712d5cd870bec023ffcbadf95d980a0
azureqaq <azureqaq@gmail.com>
2023-03-04 23:50:26 +0800

    update doubandata

--- common/src/douban.rs
+++ common/src/douban.rs
@@ -1,13 +1,14 @@
-use std::hash::Hash;
 use serde::{Deserialize, Serialize};
+use std::hash::Hash;
 
-/// 真正使用的豆瓣数据，不是原始数据
 #[derive(Debug, Serialize, Deserialize, Eq, Clone)]
 pub struct DouBanData {
     pub(crate) id: String,
     pub(crate) title: String,
     pub(crate) sub_title: Option<String>,
     pub(crate) img: String,
+    // 由于豆瓣API未提供此属性，反序列化时设为0
+    #[serde(default)]
     pub(crate) img_len: u64,
 }
 
@@ -31,6 +32,13 @@ impl DouBanData {
     pub fn img_len(&self) -> u64 {
         self.img_len
     }
+
+    /// 是否为豆瓣API返回的原始数据
+    /// 如果是，则需要获取一次
+    pub fn is_raw(&self) -> bool {
+        self.img_len == 0
+    }
+
 }
 
 impl Hash for DouBanData {
@@ -48,44 +56,5 @@ impl PartialEq for DouBanData {
 impl std::fmt::Display for DouBanData {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Data[{}-{}-{}]", self.title, self.id, self.img_len)
-    }
-}
-
-/// 豆瓣API解析豆瓣数据
-#[derive(Debug, Serialize, Deserialize, Eq, Clone)]
-pub struct RawDouBanData {
-    pub(crate) id: String,
-    pub(crate) title: String,
-    pub(crate) sub_title: Option<String>,
-    pub(crate) img: String,
-}
-
-impl RawDouBanData {
-    pub fn id(&self) -> &str {
-        &self.id
-    }
-
-    pub fn title(&self) -> &str {
-        &self.title
-    }
-
-    pub fn sub_title(&self) -> Option<&str> {
-        self.sub_title.as_deref()
-    }
-
-    pub fn img(&self) -> &str {
-        &self.img
-    }
-}
-
-impl Hash for RawDouBanData {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.id.hash(state);
-    }
-}
-
-impl PartialEq for RawDouBanData {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id
     }
 }
