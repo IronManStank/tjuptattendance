039535081b6fed622938b8e9eefed32802670216
azureqaq <azureqaq@gmail.com>
2023-03-04 12:23:59 +0800

    rebuild init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    use util::get_east_eight_now;
-    println!("{}", get_east_eight_now().format("%Y-%m-%d %H:%M:%S"));
-    println!("{}", get_east_eight_now().format("%Y-%m-%d %H:%M:%S"));
     Ok(())
 }
--- tjupt_models/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// 作为 tjuptattendance 的一部分
-// license: MIT
-
-#![warn(unreachable_pub)]
-
-mod user;
-pub use user::User;
