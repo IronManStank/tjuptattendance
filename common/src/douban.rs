4be85439094584e4a1c7c3e7e76dc26e020b7898
azureqaq <azureqaq@gmail.com>
2023-03-10 10:46:52 +0800

    add datastore

--- common/src/douban.rs
+++ common/src/douban.rs
@@ -2,13 +2,74 @@
 //! - 从豆瓣API获取
 //! - 从自建API获取
 
-use std::hash::Hash;
+use std::{
+    hash::Hash,
+    sync::{Arc, Mutex},
+};
 
+use crate::error::{DouBanDataError, Error};
+use ahash::AHashSet;
+use anyhow::anyhow;
 use lazy_static::lazy_static;
 use reqwest::{header::HeaderMap, Client, ClientBuilder};
 use serde::{Deserialize, Serialize};
 
-use crate::error::{DouBanDataError, Error};
+#[derive(Debug, Default)]
+pub struct DataStore {
+    pub(crate) inner: AHashSet<DouBanData>,
+}
+
+impl DataStore {
+    pub fn content(&self) -> &AHashSet<DouBanData> {
+        &self.inner
+    }
+
+    async fn try_update_img(&mut self) {
+        let mut set = AHashSet::default();
+        let client = get_client();
+
+        for data in self.inner.drain() {
+            match data.get_img_len(&client).await {
+                Err(e) => {
+                    eprintln!("{e}");
+                    continue;
+                }
+                Ok(d) => {
+                    set.insert(d);
+                }
+            }
+        }
+        self.inner = set;
+    }
+
+    pub async fn report_to_api(&mut self, api: &Api) -> Result<(), Error> {
+        if api.report() && !self.inner.is_empty() {
+            self.try_update_img().await;
+            let client = get_client();
+            let res = client
+                .post(&api.url)
+                .query(&[("q", api.token())])
+                .json(&self.inner)
+                .send()
+                .await?;
+            if res.status().is_success() {
+                println!("成功向 {} 汇报 {} 个结果", api, self.inner.len());
+            }
+            Ok(())
+        } else {
+            eprintln!("{api} 未开启 report 或 无可汇报数据");
+            Ok(())
+        }
+    }
+}
+
+impl std::ops::Deref for DataStore {
+    type Target = AHashSet<DouBanData>;
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+
 
 #[derive(Debug, Serialize, Deserialize, Eq, Clone)]
 pub struct DouBanData {
@@ -22,14 +83,15 @@ pub struct DouBanData {
 }
 
 impl DouBanData {
-    /// 获取图片大小
+    /// 获取图片大小，如果失败则不改变
     pub async fn get_img_len(self, client: &Client) -> Result<Self, Error> {
         if self.img_len != 0 {
             Ok(self)
         } else {
             // let client = get_client();
             let Some(img_len) = client.get(&self.img_url).send().await?.content_length() else {
-                return Err(DouBanDataError::ImgLenNotFound.into());
+                // 如果失败就返回自身
+                return Ok(self);
             };
             Ok(Self { img_len, ..self })
         }
@@ -80,9 +142,39 @@ impl Api {
         self.report.unwrap_or(false)
     }
 
-    pub async fn get_data(title: &str) -> Result<DouBanData, Error> {
-        println!("{title}");
-        todo!()
+    pub async fn get_data(
+        &self,
+        title: &str,
+        data_store: Arc<Mutex<DataStore>>,
+    ) -> Result<DouBanData, Error> {
+        let client = get_client();
+
+        let data_vec: Vec<DouBanData> = client
+            .get(&self.url)
+            .query(&[("q", Some(title)), ("t", self.token())])
+            .send()
+            .await?
+            .json()
+            .await?;
+
+        // 返回API数据
+        if self.report() && !data_vec.is_empty() {
+            let Ok(mut lock) = data_store.lock() else {
+                return Err(anyhow!("内部错误").into());
+            };
+            lock.inner
+                .extend(data_vec.clone().into_iter().filter(|d| d.img_len == 0));
+        }
+
+        let Some(data) = data_vec.into_iter().next() else {
+            return Err(DouBanDataError::ApiTired.into());
+        };
+        if data.img_len == 0 {
+            Ok(data.get_img_len(&client).await?)
+        } else {
+            // 从自建API获取的数据
+            Ok(data)
+        }
     }
 }
 
@@ -98,31 +190,9 @@ impl PartialEq for Api {
     }
 }
 
-/// 从自定义API获取
-pub async fn get_data_by_my_api(api: &Api, title: &str) -> Result<DouBanData, Error> {
-    let client = get_client();
-    let data_vec: Vec<DouBanData> = client
-        .get(&api.url)
-        .query(&[("q", Some(title)), ("t", api.token())])
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    // TODO 可以在此返回数据
-    if api.report() && data_vec.get(0).map(|d| d.img_len != 0).unwrap_or(false) {
-        // 返回API数据
-    }
-
-    let Some(data) = data_vec.into_iter().next() else {
-        return Err(DouBanDataError::ApiTired.into());
-    };
-
-    if data.img_len == 0 {
-        Ok(data.get_img_len(&client).await?)
-    } else {
-        // 从自建API获取的数据
-        Ok(data)
+impl std::fmt::Display for Api {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.url)
     }
 }
 
@@ -159,6 +229,7 @@ mod test_douban_api {
 
     #[tokio::test]
     async fn test_get_data() {
+        let store = Arc::new(Mutex::new(DataStore::default()));
         let data_origin = DouBanData {
             id: "26647087".into(),
             img_url: "https://img2.doubanio.com/view/photo/s_ratio_poster/public/p2886492021.jpg"
@@ -166,14 +237,15 @@ mod test_douban_api {
             title: "三体".into(),
             img_len: 17075,
         };
-        let data = get_data_by_my_api(&Api::new_doubanapi(), "三体")
+        let data_douban = Api::new_doubanapi()
+            .get_data(&data_origin.title, store.clone())
             .await
             .unwrap();
         // DouBanData 实现了 Eq，所以不能直接比较，而是比较其 Debug 的字符
-        assert_str_eq!(format!("{:#?}", data_origin), format!("{:#?}", data));
+        assert_str_eq!(format!("{:#?}", data_origin), format!("{:#?}", data_douban));
 
         // 测试获取图片大小
-        let data_1 = DouBanData {
+        let data_no_len = DouBanData {
             id: "26647087".into(),
             img_url: "https://img2.doubanio.com/view/photo/s_ratio_poster/public/p2886492021.jpg"
                 .into(),
@@ -181,16 +253,26 @@ mod test_douban_api {
             img_len: 0,
         };
         let clinet = get_client();
-        let data_1 = data_1.get_img_len(&clinet).await.unwrap();
-        assert_str_eq!(format!("{:#?}", data_origin), format!("{:#?}", data_1));
+        let data_with_len = data_no_len.get_img_len(&clinet).await.unwrap();
+        assert_str_eq!(
+            format!("{:#?}", data_origin),
+            format!("{:#?}", data_with_len)
+        );
 
         // 测试Myapi
         let myapi = Api {
             url: "https://movie.douban.com/j/subject_suggest".into(),
             token: None,
-            report: None,
+            report: Some(true),
         };
-        let data = get_data_by_my_api(&myapi, "三体").await.unwrap();
+        let data = myapi.get_data("三体", store.clone()).await.unwrap();
         assert_eq!(data.id, data_origin.id);
+        {
+            let mut lock = store.lock().unwrap();
+            lock.try_update_img().await;
+            assert!(lock.content().contains(&data_origin));
+
+            // TODO 测试反馈功能
+        }
     }
 }
