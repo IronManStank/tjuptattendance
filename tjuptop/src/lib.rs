f8c4613534d506ef2bf3ce154fa2dd29f93f8333
azureqaq <azureqaq@gmail.com>
2023-03-15 17:38:23 +0800

    update wait to

--- tjuptop/src/lib.rs
+++ tjuptop/src/lib.rs
@@ -12,10 +12,11 @@ pub mod user;
 use chrono::prelude::*;
 
 /// 等待至时间点
+/// 如果是 `None` or 0 or 负数 则不等待
 pub async fn wait_to_point(target: Option<NaiveDateTime>) {
     if let Some(dt) = target {
-        let dur = (dt - util::get_now()).abs();
-        if !dur.is_zero() {
+        let dur = dt - util::get_now();
+        if !dur.abs().is_zero() {
             tokio::time::sleep(dur.to_std().unwrap_or_default()).await;
         }
     }
@@ -28,17 +29,30 @@ mod test_tjuptop_util {
     /// 测试：等待到时间点
     #[tokio::test]
     async fn test_wait_to_point() {
-        let target = util::get_now() + chrono::Duration::seconds(1);
+        // 一个未来的时间点
+        let after = util::get_now() + chrono::Duration::seconds(1);
+        // 一个过去的时间点
+        let before = after - chrono::Duration::seconds(10);
+
+        // 等待到一个未来的时间点
         let ins = tokio::time::Instant::now();
-        wait_to_point(Some(target)).await;
+        wait_to_point(Some(after)).await;
         let dur = ins.elapsed().as_secs_f32();
         dbg!(dur);
-        assert!((dur - 1.0).abs() <= 0.1);
+        assert!(dur < 1.03);
 
+        // 等待一个过去的和不等待的时间点
         let ins = tokio::time::Instant::now();
         wait_to_point(None).await;
-        let dur = ins.elapsed();
+        wait_to_point(Some(before)).await;
+        let dur = ins.elapsed().as_secs_f32();
         dbg!(dur);
-        assert!(dur.as_secs_f32().abs() <= 0.01);
+        assert!(dur <= 0.03);
+
+        // 负数转换
+        let dur_neg = chrono::Duration::seconds(-20);
+        let dur_std = dur_neg.to_std().unwrap_or_default();
+        dbg!(dur_std);
+        assert!(dur_std == std::time::Duration::ZERO);
     }
 }
